8037af91b994c36cefa8393edeaf741a52b73024
John VanGeemen <johnvdev@hotmail.com>
2018-08-28 20:48:04 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
 import "./App.css";
 
@@ -6,7 +8,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>test github works</h1>
+        <Header branding="ContactManager" />
+        <Contact
+          name="John Doe"
+          email="jdoe@hotmail.com"
+          phone="555-555-5555"
+        />
+        <Contact name="karen kay" email="kk@hotmail.com" phone="555-555-5555" />
       </div>
     );
   }
