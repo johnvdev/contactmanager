c43c548afc20063614d7c56160f6afbd34af1625
John VanGeemen <johnvdev@hotmail.com>
2018-09-03 13:40:52 -0400

    added global centext, added new contact form

---
+++ src/context.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "jdoe@hotmail.com",
+        phone: "555-555-5555"
+      },
+      {
+        id: 2,
+        name: "karren will",
+        email: "karrenw@hotmail.com",
+        phone: "555-555-5555"
+      },
+      {
+        id: 3,
+        name: "jeff johnston",
+        email: "jeffj@hotmail.com",
+        phone: "555-555-5555"
+      }
+    ],
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    }
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+export const Consumer = Context.Consumer;
